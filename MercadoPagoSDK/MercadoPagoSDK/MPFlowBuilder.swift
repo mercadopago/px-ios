014534702c43b11ffbad59ca58b272f5964f2905
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2017-01-05 17:29:49 -0300

    Compilation error

--- MercadoPagoSDK/MercadoPagoSDK/MPFlowBuilder.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MPFlowBuilder.swift
@@ -146,7 +146,7 @@ open class MPFlowBuilder : NSObject {
             }, callbackCancel : currentCallbackCancel)
         
         ccf = cardVC?.viewControllers[0] as! CardFormViewController
-        
+        esto es un error
         
         cardVC!.modalTransitionStyle = .crossDissolve
         return cardVC!
