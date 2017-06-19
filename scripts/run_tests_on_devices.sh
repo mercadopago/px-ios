0bf3a9365fd5f7947ab8e4c8ea653a187906fffe
Edén Torres <eden.etorres@gmail.com>
2017-06-19 12:55:17 -0300

    Update run_tests_on_devices.sh

--- scripts/run_tests_on_devices.sh
+++ scripts/run_tests_on_devices.sh
@@ -3,7 +3,7 @@
 cd ../MercadoPagoSDK
 
 # xcodebuild clean test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6" 
-xcodebuild clean test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s" ONLY_ACTIVE_ARCH=NO
+# xcodebuild clean test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s" ONLY_ACTIVE_ARCH=NO
 
-# xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6"
-# xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s"
+xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6"
+xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s"
