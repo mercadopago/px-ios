a4f8224249f2714bad44dd425f6e962677f4895d
Eden Torres <eden.etorres@gmail.com>
2017-06-19 09:54:14 -0300

    Update travis.yml and test file

--- scripts/run_tests_on_devices.sh
+++ scripts/run_tests_on_devices.sh
@@ -2,6 +2,8 @@
 
 cd ../MercadoPagoSDK
 
-xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6"
+xcodebuild clean test -project MercadoPagoSDK.xcodeproj -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,name=iPhone 7,OS=10.3" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO -quiet
+xcodebuild clean test -project MercadoPagoSDK.xcodeproj -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,name=iPhone 7,OS=8.2" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO -quiet
 
-xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s"
+# xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6"
+# xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s"
