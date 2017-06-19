9d39e31caae61f3c37793c1ec726d4e3c2983130
Eden Torres <eden.etorres@gmail.com>
2017-06-19 10:49:14 -0300

    Update travis.yml and test file

--- scripts/run_tests_on_devices.sh
+++ scripts/run_tests_on_devices.sh
@@ -2,8 +2,8 @@
 
 cd ../MercadoPagoSDK
 
-xcodebuild clean test -project MercadoPagoSDK.xcodeproj -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,name=iPhone SE,OS=10.1" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO -quiet | grep error
-xcodebuild clean test -project MercadoPagoSDK.xcodeproj -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,name=iPhone 6S,OS=9.0" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO -quiet | grep error
+xcodebuild clean test -project MercadoPagoSDK.xcodeproj -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,name=iPhone SE,OS=10.1" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO -quiet
+xcodebuild clean test -project MercadoPagoSDK.xcodeproj -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,name=iPhone 6S,OS=9.0" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO -quiet
 
 # xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6"
 # xcodebuild test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s"
