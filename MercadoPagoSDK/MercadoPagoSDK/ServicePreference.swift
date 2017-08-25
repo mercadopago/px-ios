ce91fcac553cd2cd92cfd48e00436654086cf6fa
Eden Torres <eden.etorres@gmail.com>
2017-08-25 11:48:34 -0300

    Change to wrapper endpoint

--- MercadoPagoSDK/MercadoPagoSDK/ServicePreference.swift
+++ MercadoPagoSDK/MercadoPagoSDK/ServicePreference.swift
@@ -40,22 +40,29 @@ open class ServicePreference: NSObject {
 
     static let MP_API_BASE_URL: String =  MP_API_BASE_URL_PROD
 
-    static let MP_CUSTOMER_URI = "/customers?preference_id="
-    static let MP_PAYMENTS_URI = MP_ENVIROMENT + "/payments"
+    static let PAYMENT_METHODS = "/payment_methods"
+    static let INSTALLMENTS = "\(PAYMENT_METHODS)/installments"
+    static let CARD_TOKEN = "/card_tokens"
+    static let CARD_ISSSUERS = "\(PAYMENT_METHODS)/card_issuers"
+    static let PAYMENTS = "/payments"
 
-    static let MP_CREATE_TOKEN_URI = MP_OP_ENVIROMENT + "/card_tokens"
-    static let MP_PAYMENT_METHODS_URI = MP_OP_ENVIROMENT + "/payment_methods"
-    static let MP_INSTALLMENTS_URI = MP_OP_ENVIROMENT + "/payment_methods/installments"
-    static let MP_ISSUERS_URI = MP_OP_ENVIROMENT + "/payment_methods/card_issuers"
+    static let MP_CREATE_TOKEN_URI = MP_OP_ENVIROMENT + CARD_TOKEN
+    static let MP_PAYMENT_METHODS_URI = MP_OP_ENVIROMENT + PAYMENT_METHODS
+    static var MP_INSTALLMENTS_URI = MP_OP_ENVIROMENT + INSTALLMENTS
+    static var MP_ISSUERS_URI = MP_OP_ENVIROMENT + CARD_ISSSUERS
     static let MP_IDENTIFICATION_URI = "/identification_types"
-    static let MP_PROMOS_URI = MP_OP_ENVIROMENT + "/payment_methods/deals"
-    static let MP_SEARCH_PAYMENTS_URI = MP_ENVIROMENT + "/payment_methods/search/options"
-    static let MP_INSTRUCTIONS_URI = MP_ENVIROMENT + "/payments/${payment_id}/results"
+    static let MP_PROMOS_URI = MP_OP_ENVIROMENT + PAYMENT_METHODS + "/deals"
+    static let MP_SEARCH_PAYMENTS_URI = MP_ENVIROMENT + PAYMENT_METHODS + "/search/options"
+    static let MP_INSTRUCTIONS_URI = MP_ENVIROMENT + PAYMENTS + "/${payment_id}/results"
     static let MP_PREFERENCE_URI = MP_ENVIROMENT + "/preferences/"
     static let MP_DISCOUNT_URI =  "/discount_campaigns/"
     static let MP_TRACKING_EVENTS_URI =  MP_ENVIROMENT + "/tracking/events"
+    static let MP_CUSTOMER_URI = "/customers?preference_id="
+    static let MP_PAYMENTS_URI = MP_ENVIROMENT + PAYMENTS
 
     private static let kIsProdApiEnvironemnt = "prod_mp_api_environment"
+    private static let kShouldGoToWrapperInstallments = "go_to_wrapper_installments"
+    private static let kShouldGoToWrapperIssuers = "go_to_wrapper_issuers"
 
     private var useDefaultPaymentSettings = true
     private var defaultDiscountSettings = true
@@ -215,7 +222,27 @@ open class ServicePreference: NSObject {
             } else {
                 ServicePreference.MP_ENVIROMENT = MP_TEST_ENV  + "/checkout"
             }
+
+            let shouldGoToWrapperInstallments: Bool = Utils.getSetting(identifier: ServicePreference.kShouldGoToWrapperInstallments)
+
+            if shouldGoToWrapperInstallments {
+                ServicePreference.changeInstallmensEnv()
+            }
+
+            let shouldGoToWrapperIssuers: Bool = Utils.getSetting(identifier: ServicePreference.kShouldGoToWrapperIssuers)
+
+            if shouldGoToWrapperIssuers {
+                ServicePreference.changeIssuersEnv()
+            }
         }
+    }
+
+    static func changeInstallmensEnv() {
+        ServicePreference.MP_INSTALLMENTS_URI = MP_ENVIROMENT + INSTALLMENTS
+    }
+
+    static func changeIssuersEnv() {
+        ServicePreference.MP_ISSUERS_URI = MP_ENVIROMENT + CARD_ISSSUERS
     }
 
     public func getProcessingModeString() -> String {
@@ -229,15 +256,15 @@ open class ServicePreference: NSObject {
     public func setGatewayAsProcessingMode() {
         self.processingMode = ProcessingMode.gateway
     }
-    
+
     public func setHybridAsProcessingMode() {
         self.processingMode = ProcessingMode.hybrid
     }
-    
+
     internal func shouldShowBankDeals() -> Bool {
         return self.processingMode == ProcessingMode.aggregator
     }
- 
+
 }
 
 public enum ProcessingMode: String {
