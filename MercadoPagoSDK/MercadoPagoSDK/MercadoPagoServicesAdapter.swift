eb0f9cb3158c7e4fcc8664c4d860d30aa5e278f9
Augusto Collerone <augusto.collerone@mercadolibre.com>
2017-10-23 14:49:16 -0300

    MercadoPagoServicesAdapter Created

---
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoServicesAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  MercadoPagoServicesAdapter.swift
+//  MercadoPagoSDK
+//
+//  Created by AUGUSTO COLLERONE ALFONSO on 10/23/17.
+//  Copyright © 2017 MercadoPago. All rights reserved.
+//
+
+import Foundation
+
+open class MercadoPagoServicesAdapter: NSObject {
+    
+}
