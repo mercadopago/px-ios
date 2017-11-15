3e7a181f13621268b641ac29d2590244f3fd220a
Eden Torres <eden.etorres@gmail.com>
2017-11-14 23:47:47 -0300

    Fix travis

--- scripts/run_tests_on_devices.sh
+++ scripts/run_tests_on_devices.sh
@@ -2,6 +2,6 @@
 
 cd ../MercadoPagoSDK
 
-xcodebuild clean test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6" -quiet | xcpretty -t
+xcodebuild clean test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=9.3,name=iPhone 6"
 
-xcodebuild clean test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s" -quiet
+xcodebuild clean test -scheme MercadoPagoSDKTests -destination "platform=iOS Simulator,OS=10.0,name=iPhone 6s" -quiet | xcpretty -t
