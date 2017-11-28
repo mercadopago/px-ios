40e05fe5dd5f00ce5b709f9b4e5e9f61e758d3af
Juan Sanzone <juan.sanzone@gmail.com>
2017-11-28 09:49:48 -0300

    Hook real example UI/config finished

--- MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC/Hooks/FirstHookViewController.m
+++ MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC/Hooks/FirstHookViewController.m
@@ -12,7 +12,8 @@ id <PaymentMethodOption> paymentOptionSelected;
 
 @interface FirstHookViewController ()
 
-@property (weak, nonatomic) IBOutlet UILabel *paymentType;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+@property (weak, nonatomic) IBOutlet UITextField *codeTextField;
 
 @end
 
@@ -21,25 +22,60 @@ id <PaymentMethodOption> paymentOptionSelected;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"FirstHookViewController loaded");
+    [self setupNextButton];
 }
 
-- (IBAction)didTapOnNext {
-    if (self.actionHandler != nil) {
+#pragma mark - Setup methods
+- (void)setupNextButton {
     
-        // Loading example
-        [self.actionHandler showLoading];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    [button addTarget:self
+     action:@selector(didTapOnNext)
+     forControlEvents:UIControlEventTouchUpInside];
+    [button setBackgroundColor:[UIColor fromHex:@"#CA254D"]];
+    [button setTintColor:UIColor.whiteColor];
+    [button setTitle:@"Continuar" forState:UIControlStateNormal];
+    button.frame = CGRectMake(0, -4, self.view.bounds.size.width, 55.0);
+    
+    _codeTextField.inputAccessoryView = button;
+}
+
+#pragma mark - Selectors/handlers
+- (IBAction)didTapOnNext {
+    
+    if (self.actionHandler != nil) {
         
-        double delay = 3.0;
-        dispatch_time_t tm = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC));
-        dispatch_after(tm, dispatch_get_main_queue(), ^(void){
-            // Hide loading and back action example
-            [self.actionHandler hideLoading];
-            [self.actionHandler back];
-        });
+        _messageLabel.text = nil;
+        
+        if  ([self codeIsValid]) {
+            
+            // Loading example
+            [self.actionHandler showLoading];
+            
+            double delay = 3.0;
+            dispatch_time_t tm = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC));
+            dispatch_after(tm, dispatch_get_main_queue(), ^(void){
+                // Hide loading and back action example
+                [self.actionHandler hideLoading];
+                [self.actionHandler back];
+            });
+        }
     }
 }
 
+- (BOOL)codeIsValid {
+    if (self.codeTextField.text.length > 0) {
+        if ([self.codeTextField.text isEqualToString:@"1234"]) return true;
+        self.messageLabel.text = @"La clave no es válida.";
+        return false;
+    } else {
+        self.messageLabel.text = @"Debes completar este dato.";
+        return false;
+    }
+}
+
+#pragma mark - Hookeable delegates
 - (UIView * _Nonnull)render {
     return self.view;
 }
@@ -53,8 +89,10 @@ id <PaymentMethodOption> paymentOptionSelected;
 }
 
 - (void)renderDidFinish {
-    self.paymentType.text = [paymentOptionSelected getDescription];
-    NSLog(@"renderDidFinish");
+    //self.paymentType.text = [paymentOptionSelected getDescription];
+    self.messageLabel.text = nil;
+    self.codeTextField.text = nil;
+    [self.codeTextField becomeFirstResponder];
 }
 
 - (BOOL)shouldShowBackArrow {
@@ -70,7 +108,7 @@ id <PaymentMethodOption> paymentOptionSelected;
 }
 
 - (UIColor * _Nullable)colorForNavigationBar {
-    return UIColor.mpDefaultColor;
+    return [UIColor fromHex:@"#CA254D"];
 }
 
 @end
