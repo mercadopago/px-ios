6697d7a9274720c060f3561e63bc59873c5b334d
augustocollerone <collerone.augusto@gmail.com>
2018-03-22 12:21:21 -0300

    Test script

---
+++ scripts/test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+
+pwd
+
+value=$(<./MercadoPagoSDK/translations.plist)
+echo "$value"
+
+echo "Clearing PXStrings Content"
+cat /dev/null >| ./MercadoPagoSDK/PXStrin.swift
+
+cat <<EOT >> ./MercadoPagoSDK/PXStrin.swift
+//
+//  PXStrings.swift
+//  MercadoPagoSDK
+//
+//
+//  Copyright © 2018 MercadoPago. All rights reserved.
+//
+
+import Foundation
+
+enum PXStrings {
+    static let error_body_title_ = "error_body_title_"
+    static let error_body_description_ = "error_body_description_"
+    static let error_body_description_pending_contingency = "error_body_description_pending_contingency"
+    static let error_body_description_pending_review_manual = "error_body_description_pending_review_manual"
+    static let error_body_description_cc_rejected_call_for_authorize = "error_body_description_cc_rejected_call_for_authorize"
+    static let error_body_description_cc_rejected_card_disabled = "error_body_description_cc_rejected_card_disabled"
+    static let error_body_description_cc_rejected_insufficient_amount = "error_body_description_cc_rejected_insufficient_amount"
+    static let error_body_description_cc_rejected_other_reason = "error_body_description_cc_rejected_other_reason"
+    static let error_body_description_rejected_by_bank = "error_body_description_rejected_by_bank"
+    static let error_body_description_rejected_insufficient_data = "error_body_description_rejected_insufficient_data"
+    static let error_body_description_cc_rejected_duplicated_payment = "error_body_description_cc_rejected_duplicated_payment"
+    static let error_body_description_cc_rejected_max_attempts = "error_body_description_cc_rejected_max_attempts"
+    static let error_body_description_rejected_high_risk = "error_body_description_rejected_high_risk"
+    static let error_body_action_text_ = "error_body_action_text_"
+    static let error_body_action_text_cc_rejected_call_for_authorize = "error_body_action_text_cc_rejected_call_for_authorize"
+    static let error_body_secondary_title_ = "error_body_secondary_title_"
+    static let discount_coupon_detail_default_concept = "discount_coupon_detail_default_concept"
+    static let discount_coupon_detail_description = "discount_coupon_detail_description"
+    static let payment_result_screen_congrats_finish_button = "payment_result_screen_congrats_finish_button"
+    static let card_form_next_button = "card_form_next_button"
+    static let card_form_previous_button = "card_form_previous_button"
+    static let payer_info_name = "payer_info_name"
+    static let payer_info_surname = "payer_info_surname"
+    static let payer_info_number = "payer_info_number"
+    static let payer_info_type = "payer_info_type"
+    static let review_change_payment_method_action = "review_change_payment_method_action"
+EOT
+
+for number in {1..14}
+do
+  echo "    static let numero_$number =  "$number"" >> ./MercadoPagoSDK/PXStrin.swift
+done
+echo "}" >> ./MercadoPagoSDK/PXStrin.swift
+
+exit 0
