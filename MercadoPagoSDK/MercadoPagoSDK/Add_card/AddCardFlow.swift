9c101342066f95631703775e3883f8c2ba40f111
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2019-08-20 10:23:57 -0300

    Refactor de setSite en AddCardFlow

--- MercadoPagoSDK/MercadoPagoSDK/Add_card/AddCardFlow.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Add_card/AddCardFlow.swift
@@ -46,7 +46,8 @@ public class AddCardFlow: NSObject, PXFlow {
     }
 
     public func setSiteId(_ siteId: String) {
-        SiteManager.shared.setSite(siteId: siteId)
+        let siteFactory = AddCardFlowSiteFactory()
+        SiteManager.shared.setSite(site: siteFactory.createSite(siteId))
     }
 
     public func start() {
