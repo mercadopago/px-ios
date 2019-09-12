ca0fdf26d0d47e5f9087b4b66d1c0db72f0a926e
Eric Ertl <eric.ertl@mercadolibre.com>
2019-09-12 16:22:15 -0300

    More fixes

--- MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/OneTapFlow+Screens.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/OneTapFlow+Screens.swift
@@ -13,35 +13,32 @@ extension OneTapFlow {
         let reviewVC = PXOneTapViewController(viewModel: model.reviewConfirmViewModel(), timeOutPayButton: model.getTimeoutForOneTapReviewController(), callbackPaymentData: { [weak self] (paymentData: PXPaymentData) in
             self?.cancelFlowForNewPaymentSelection()
             return
-            }, callbackConfirm: {(paymentData: PXPaymentData, splitAccountMoneyEnabled: Bool) in
-                self.model.updateCheckoutModel(paymentData: paymentData, splitAccountMoneyEnabled: splitAccountMoneyEnabled)
+            }, callbackConfirm: { [weak self] (paymentData: PXPaymentData, splitAccountMoneyEnabled: Bool) in
+                self?.model.updateCheckoutModel(paymentData: paymentData, splitAccountMoneyEnabled: splitAccountMoneyEnabled)
                 // Deletes default one tap option in payment method search
-                self.executeNextStep()
-        }, callbackUpdatePaymentOption: { [weak self] (newPaymentOption: PaymentMethodOption) in
-            if let card = newPaymentOption as? PXCardSliderViewModel, let newPaymentOptionSelected = self?.getCustomerPaymentOption(forId: card.cardId ?? "") {
-                // Customer card.
-                self?.model.paymentOptionSelected = newPaymentOptionSelected
-            } else if newPaymentOption.getId() == PXPaymentTypes.ACCOUNT_MONEY.rawValue ||
-                newPaymentOption.getId() == PXPaymentTypes.CONSUMER_CREDITS.rawValue {
-                //AM
-                self?.model.paymentOptionSelected = newPaymentOption
-            }
-        }, callbackExit: { [weak self] () -> Void in
-            guard let strongSelf = self else {
-                return
-            }
-            strongSelf.cancelFlow()
-            }, finishButtonAnimation: {
-                self.executeNextStep()
+                self?.executeNextStep()
+            }, callbackUpdatePaymentOption: { [weak self] (newPaymentOption: PaymentMethodOption) in
+                if let card = newPaymentOption as? PXCardSliderViewModel, let newPaymentOptionSelected = self?.getCustomerPaymentOption(forId: card.cardId ?? "") {
+                    // Customer card.
+                    self?.model.paymentOptionSelected = newPaymentOptionSelected
+                } else if newPaymentOption.getId() == PXPaymentTypes.ACCOUNT_MONEY.rawValue ||
+                    newPaymentOption.getId() == PXPaymentTypes.CONSUMER_CREDITS.rawValue {
+                    //AM
+                    self?.model.paymentOptionSelected = newPaymentOption
+                }
+            }, callbackExit: { [weak self] in
+                self?.cancelFlow()
+            }, finishButtonAnimation: { [weak self] in
+                self?.executeNextStep()
         })
 
-        self.pxNavigationHandler.pushViewController(viewController: reviewVC, animated: true)
+        pxNavigationHandler.pushViewController(viewController: reviewVC, animated: true)
     }
 
     func showSecurityCodeScreen() {
         let securityCodeVc = SecurityCodeViewController(viewModel: model.savedCardSecurityCodeViewModel(), collectSecurityCodeCallback: { [weak self] (_, securityCode: String) -> Void in
             self?.getTokenizationService().createCardToken(securityCode: securityCode)
         })
-        self.pxNavigationHandler.pushViewController(viewController: securityCodeVc, animated: true)
+        pxNavigationHandler.pushViewController(viewController: securityCodeVc, animated: true)
     }
 }
