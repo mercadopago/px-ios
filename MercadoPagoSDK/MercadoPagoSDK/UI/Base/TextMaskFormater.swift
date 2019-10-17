91eb413c49e964458559bb7a7800e26c4b0bba85
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2019-10-17 12:40:00 -0300

    Improvements

--- MercadoPagoSDK/MercadoPagoSDK/UI/Base/TextMaskFormater.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/Base/TextMaskFormater.swift
@@ -41,10 +41,7 @@ internal class TextMaskFormater {
             let charset: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
             var ints: String = ""
             for char: Character in text {
-                if charset.contains(char) {
-                    ints.append(char)
-                } else if isLetter(char) {
-                    // Fix like verified digit RUT CL.
+                if charset.contains(char) || isLetter(char) {
                     ints.append(char)
                 }
             }
