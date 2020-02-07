7f8999ec6acbf4ad96552c85c0110a0ccd6b7a17
Vicente Veltri <vicente.veltri@mercadolibre.com>
2020-02-07 11:53:51 -0300

    Discount tracker implementation.

--- MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXDiscountTracker.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXDiscountTracker.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import MLBusinessComponents
+
+public class PXDiscountTracker: NSObject, MLBusinessDiscountTrackerProtocol {
+    
+    let basePath = "/discount_center/payers/touchpoint"
+    var touchPointId: String
+    
+    init(touchPointId: String) {
+        self.touchPointId = touchPointId
+    }
+    
+    public func track(action: String, eventData: [[String : Any]]) {
+        let path = "\(basePath)/\(touchPointId)/\(action)"
+        var properties: [String: Any] = [:]
+        
+        for dictionary in eventData {
+            for item in dictionary {
+                properties[item.key] = item.value
+            }
+        }
+        
+        MPXTracker.sharedInstance.trackEvent(path: path, properties: properties)
+    }
+    
+}
