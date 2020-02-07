a1c5b5692e647ee3c8660e59f2f456346467e768
Vicente Veltri <vicente.veltri@mercadolibre.com>
2020-02-07 15:07:24 -0300

    Fixed eventDataForItem method.

--- MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXDiscountItemData.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXDiscountItemData.swift
@@ -36,14 +36,7 @@ class PXDiscountItemData: NSObject, MLBusinessSingleItemProtocol {
     }
     
     func eventDataForItem() -> [String : Any]? {
-        var eventData: [String : Any] = [ : ]
-        
-        eventData["title"] = item.title
-        eventData["subtitle"] = item.subtitle
-        eventData["icon"] = item.icon
-        if let target = item.target { eventData["target"] = target }
-        if let campaignId = item.campaingId { eventData["campaignId"] = campaignId }
-
-        return eventData
+        guard let campaignId = item.campaingId else { return nil }
+        return ["tracking_id" : campaignId ]
     }
 }
