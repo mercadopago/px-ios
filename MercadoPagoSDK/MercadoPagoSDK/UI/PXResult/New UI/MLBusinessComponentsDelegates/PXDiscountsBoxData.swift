af62eff3247a643c25c3f3ccd1c3d8c45556bb3e
Vicente Veltri <vicente.veltri@mercadolibre.com>
2020-02-07 12:09:43 -0300

    Updated tracking id name.

--- MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXDiscountsBoxData.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/MLBusinessComponentsDelegates/PXDiscountsBoxData.swift
@@ -33,6 +33,6 @@ class PXDiscountsBoxData: NSObject, MLBusinessDiscountBoxData {
     }
     
     func getDiscountTracker() -> MLBusinessDiscountTrackerProtocol? {
-        return PXDiscountTracker(touchPointId: "congrats-mp")
+        return PXDiscountTracker(touchPointId: "px-congrats")
     }
 }
