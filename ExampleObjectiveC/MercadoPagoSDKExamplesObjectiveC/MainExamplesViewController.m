f49b8e83d36947a01018a152d07c78a1daca749b
Eric Ertl <eric.ertl@mercadolibre.com>
2020-06-12 09:52:26 -0300

    Changes for appium

--- ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
+++ ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
@@ -35,7 +35,7 @@
 //    [self.checkoutBuilder setPrivateKeyWithKey:@"APP_USR-1505-092415-b89a7cdcec6cc6c3916deab0c56c7136-472129472"];
 
     //  Argentina
-    [self.checkoutBuilder setPrivateKeyWithKey:@"APP_USR-7092-091314-cc8f836a12b9bf78b16e77e4409ed873-470735636"];
+    [self.checkoutBuilder setPrivateKeyWithKey:@"TEST-1693017181771849-122314-273743ae06895406bb9759d6ead36a35-502374722"];
 
     PXTrackingConfiguration *trackingConfig = [[PXTrackingConfiguration alloc] initWithTrackListener: self flowName:@"instore" flowDetails:nil sessionId:@"3783874"];
     [self.checkoutBuilder setTrackingConfigurationWithConfig: trackingConfig];
