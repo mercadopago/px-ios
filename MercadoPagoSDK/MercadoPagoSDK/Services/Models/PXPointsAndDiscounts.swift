eeac1bc690c06be81d95ee9a2b90b30436141ed9
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-06-24 11:34:38 -0300

    Add improvements

--- MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXPointsAndDiscounts.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXPointsAndDiscounts.swift
@@ -17,14 +17,14 @@ struct PXPointsAndDiscounts: Decodable {
     let customOrder: Bool?
     let expenseSplit: PXExpenseSplit?
 
-    init(points: PXPoints?, discounts: PXDiscounts?, crossSelling: [PXCrossSellingItem]?, viewReceiptAction: PXRemoteAction?, topTextBox: PXText?, customOrder: Bool?, paymentSplit: PXExpenseSplit?) {
+    init(points: PXPoints?, discounts: PXDiscounts?, crossSelling: [PXCrossSellingItem]?, viewReceiptAction: PXRemoteAction?, topTextBox: PXText?, customOrder: Bool?, expenseSplit: PXExpenseSplit?) {
         self.points = points
         self.discounts = discounts
         self.crossSelling = crossSelling
         self.viewReceiptAction = viewReceiptAction
         self.topTextBox = topTextBox
         self.customOrder = customOrder
-        self.expenseSplit = paymentSplit
+        self.expenseSplit = expenseSplit
     }
 
     enum PointsAndDiscountsCodingKeys: String, CodingKey {
@@ -34,7 +34,7 @@ struct PXPointsAndDiscounts: Decodable {
         case viewReceiptAction = "view_receipt"
         case topTextBox = "top_text_box"
         case customOrder = "custom_order"
-        case paymentSplit = "expense_split"
+        case expenseSplit = "expense_split"
     }
 
     init(from decoder: Decoder) throws {
@@ -45,7 +45,7 @@ struct PXPointsAndDiscounts: Decodable {
         let viewReceiptAction: PXRemoteAction? = try container.decodeIfPresent(PXRemoteAction.self, forKey: .viewReceiptAction)
         let topTextBox: PXText? = try container.decodeIfPresent(PXText.self, forKey: .topTextBox)
         let customOrder: Bool? = try container.decodeIfPresent(Bool.self, forKey: .customOrder)
-        let paymentSplit: PXExpenseSplit? = try container.decodeIfPresent(PXExpenseSplit.self, forKey: .paymentSplit)
-        self.init(points: points, discounts: discounts, crossSelling: crossSelling, viewReceiptAction: viewReceiptAction, topTextBox: topTextBox, customOrder: customOrder, paymentSplit: paymentSplit)
+        let expenseSplit: PXExpenseSplit? = try container.decodeIfPresent(PXExpenseSplit.self, forKey: .expenseSplit)
+        self.init(points: points, discounts: discounts, crossSelling: crossSelling, viewReceiptAction: viewReceiptAction, topTextBox: topTextBox, customOrder: customOrder, expenseSplit: expenseSplit)
     }
 }
