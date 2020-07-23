a8b032ceaa23655fe2ee6965d5a5f004a8849b54
Franco Risma <franco.risma@mercadolibre.com>
2020-07-23 15:27:32 -0300

    Adding objc app congrats launcher

--- ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
+++ ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
@@ -12,6 +12,25 @@
   
 @implementation MainExamplesViewController
 
+- (IBAction)launchCongrats:(id)sender {
+    PXPaymentCongrats *congratsData = [[[[[[[[[[[[PXPaymentCongrats alloc] init]
+                                                setStatus:PXBusinessResultStatusAPPROVED]
+                                               setHeaderTitle:@"¡Listo! Ya le pagaste a SuperMarket"] //Falta header image
+                                              shouldShowReceipt:TRUE receiptId:@"123"]
+                                             setMainActionWithLabel:@"Continuar" action:^{
+        NSLog(@"Continuar");
+    }]
+                                            setSecondaryActionWithLabel:@"Tuve un problema" action:^{
+        NSLog(@"Tuve un problema");
+    }]
+                                           addPointsDataWithPercentage:0.85 levelColor:@"#4063EA" levelNumber:4 title:@"Ud ganó 2.000 puntos" actionLabel:@"Ver mis beneficios" actionTarget:@"meli://loyalty/webview?url=https%3A%2F%2Fwww.mercadolivre.com.br%2Fmercado-pontos%2Fv2%2Fhub%23origin%3Dcongrats"]
+                                          addDiscounts]
+                                         addCrossSelling]
+                                        addExpenseSplit:@"Expense"]
+                                       startUsing:self.navigationController];
+    ;
+}
+
 - (IBAction)checkoutFlow:(id)sender {
 
     //CHECKOUT PREFERENCE
