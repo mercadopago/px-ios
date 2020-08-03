489a961a37018051f104ef1c0ca94dc90f4fcdb9
DanielAlexanderSilva-meli <danielalexander.silva@mercadolibre.com>
2020-08-03 16:31:56 -0300

    Payment Congrats Add crosselling and discounts implementation (#2272)
    * add crossseling and discounts
    
    * change some names, remove data word
    
    * pass the entire object in the functions parameter, in discounts and points
    
    * added new inits
    
    * change crosseling parameter name

--- MercadoPagoSDK/MercadoPagoSDK/Models/PXPaymentCongrats.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Models/PXPaymentCongrats.swift
@@ -45,8 +45,6 @@ public final class PXPaymentCongrats: NSObject {
     // View Receipt action
     private(set) var viewReceiptAction: PXRemoteAction?
     
-    private(set) var topTextBox: PXText?
-    
     private(set) var hasCustomOrder: Bool?
     /* --- Ponints & Discounts --- */
     
@@ -148,7 +146,7 @@ extension PXPaymentCongrats {
     
     /**
      Top left close button configuration.
-     - parameter action: a colsure to excecute
+     - parameter action: a closure to excecute
      - returns: tihs builder `PXPaymentCongrats`
     */
     @discardableResult
@@ -159,8 +157,8 @@ extension PXPaymentCongrats {
 
     /**
      Defines if the receipt view should be shown, in affirmative case, the receiptId must be supplied.
-     - parameter shouldShow: some message
-     - parameter receiptId: some message
+     - parameter shouldShow: the boolean value that defines if the view will be show or not.
+     - parameter receiptId: ID of the receipt
      - returns: tihs builder `PXPaymentCongrats`
     */
     @discardableResult
@@ -171,21 +169,24 @@ extension PXPaymentCongrats {
     }
     
     /**
-     - ToDo: Fill this
+      Defines the points data in the points seccions of the congrats.
+     - parameter points: some PXPoints
+     - returns: tihs builder `PXPaymentCongrats`
     */
     @discardableResult
-    public func withPointsData(percentage: Double, levelColor: String, levelNumber: Int, title: String, actionLabel: String, actionTarget: String) -> PXPaymentCongrats {
-        let action = PXRemoteAction(label: actionLabel, target: actionTarget)
-        self.points = PXPoints(progress: PXPointsProgress(percentage: percentage, levelColor: levelColor, levelNumber: levelNumber), title: title, action: action)
+    public func withPoints(_ points: PXPoints) -> PXPaymentCongrats {
+        self.points = points
         return self
     }
 
     /**
-     - ToDo: Fill this
+     Defines the discounts data in the discounts seccions of the congrats.
+     - parameter discounts: some PXDiscounts
+     - returns: tihs builder `PXPaymentCongrats`
     */
     @discardableResult
-    public func withDiscountsData() -> PXPaymentCongrats {
-        self.discounts = PXDiscounts(title: "Descuentos por tu nivel", subtitle: "", discountsAction: PXRemoteAction(label: "Ver todos los descuentos", target: "mercadopago://discount_center_payers/list#from=/px/congrats"), downloadAction: PXDownloadAction(title: "Exclusivo con la app de Mercado Libre", action: PXRemoteAction(label: "Descargar", target: "https://852u.adj.st/discount_center_payers/list?adjust_t=ufj9wxn&adjust_deeplink=mercadopago%3A%2F%2Fdiscount_center_payers%2Flist&adjust_label=px-ml")), items: [PXDiscountsItem(icon: "https://mla-s1-p.mlstatic.com/766266-MLA32568902676_102019-O.jpg", title: "Hasta", subtitle: "20 % OFF", target: "mercadopago://discount_center_payers/detail?campaign_id=1018483&user_level=1&mcc=1091102&distance=1072139&coupon_used=false&status=FULL&store_id=13040071&sections=%5B%7B%22id%22%3A%22header%22%2C%22type%22%3A%22header%22%2C%22content%22%3A%7B%22logo%22%3A%22https%3A%2F%2Fmla-s1-p.mlstatic.com%2F766266-MLA32568902676_102019-O.jpg%22%2C%22title%22%3A%22At%C3%A9%20R%24%2010%22%2C%22subtitle%22%3A%22Nutty%20Bavarian%22%7D%7D%5D#from=/px/congrats", campaingId: "1018483"),PXDiscountsItem(icon: "https://mla-s1-p.mlstatic.com/826105-MLA32568902631_102019-O.jpg", title: "Hasta", subtitle: "20 % OFF", target: "mercadopago://discount_center_payers/detail?campaign_id=1018457&user_level=1&mcc=4771701&distance=543968&coupon_used=false&status=FULL&store_id=30316240&sections=%5B%7B%22id%22%3A%22header%22%2C%22type%22%3A%22header%22%2C%22content%22%3A%7B%22logo%22%3A%22https%3A%2F%2Fmla-s1-p.mlstatic.com%2F826105-MLA32568902631_102019-O.jpg%22%2C%22title%22%3A%22At%C3%A9%20R%24%2015%22%2C%22subtitle%22%3A%22Drogasil%22%7D%7D%5D#from=/px/congrats", campaingId: "1018457"),PXDiscountsItem(icon: "https://mla-s1-p.mlstatic.com/761600-MLA32568902662_102019-O.jpg", title: "Hasta", subtitle: "10 % OFF", target:  "mercadopago://discount_center_payers/detail?campaign_id=1018475&user_level=1&mcc=5611201&distance=654418&coupon_used=false&status=FULL&store_id=30108872&sections=%5B%7B%22id%22%3A%22header%22%2C%22type%22%3A%22header%22%2C%22content%22%3A%7B%22logo%22%3A%22https%3A%2F%2Fmla-s1-p.mlstatic.com%2F761600-MLA32568902662_102019-O.jpg%22%2C%22title%22%3A%22At%C3%A9%20R%24%2010%22%2C%22subtitle%22%3A%22McDonald%5Cu0027s%22%7D%7D%5D#from=/px/congrats", campaingId:"1018475") ], touchpoint: nil)
+    public func withDiscounts(_ discounts: PXDiscounts) -> PXPaymentCongrats {
+        self.discounts = discounts
         return self
     }
     
@@ -200,11 +201,13 @@ extension PXPaymentCongrats {
     }
 
     /**
-     - ToDo: Fill this
+     Defines the cross selling data in the cross selling seccions of the congrats.
+     - parameter crossSellingItems: an array of PXCrossSellingItem
+     - returns: tihs builder `PXPaymentCongrats`
     */
     @discardableResult
-    public func withCrossSellingData() -> PXPaymentCongrats {
-        self.crossSelling = [PXCrossSellingItem(title: "Gane 200 pesos por sus pagos diarios", icon: "https://mobile.mercadolibre.com/remote_resources/image/merchengine_mgm_icon_ml?density=xxhdpi&locale=es_AR", contentId: "cross_selling_mgm_ml", action: PXRemoteAction(label: "Invita a más amigos a usar la aplicación", target: "meli://invite/wallet"))]
+    public func withCrossSelling(_ items: [PXCrossSellingItem] ) -> PXPaymentCongrats {
+        self.crossSelling = items
         return self
     }
     
@@ -212,17 +215,8 @@ extension PXPaymentCongrats {
      - ToDo: Fill this
     */
     @discardableResult
-    public func withViewReceiptAction(label: String, target: String) -> PXPaymentCongrats {
-        self.viewReceiptAction = PXRemoteAction(label: label, target: target)
-        return self
-    }
-    
-    /**
-     - ToDo: Fill this
-    */
-    @discardableResult
-    public func withTopTextBox(message: String?, backgroundColor: String?, textColor: String?, weight: String?) -> PXPaymentCongrats {
-        self.topTextBox = PXText(message: message, backgroundColor: backgroundColor, textColor: textColor, weight: weight)
+    public func withViewReceiptAction(action: PXRemoteAction) -> PXPaymentCongrats {
+        self.viewReceiptAction = action
         return self
     }
     
@@ -257,7 +251,7 @@ extension PXPaymentCongrats {
     /**
      Top button configuration.
      - parameter label: button display text
-     - parameter action: a colsure to excecute
+     - parameter action: a closure to excecute
      - returns: tihs builder `PXPaymentCongrats`
     */
     @discardableResult
@@ -269,7 +263,7 @@ extension PXPaymentCongrats {
     /**
      Bottom button configuration.
      - parameter label: button display text
-     - parameter action: a colsure to excecute
+     - parameter action: a closure to excecute
      - returns: tihs builder `PXPaymentCongrats`
     */
     @discardableResult
