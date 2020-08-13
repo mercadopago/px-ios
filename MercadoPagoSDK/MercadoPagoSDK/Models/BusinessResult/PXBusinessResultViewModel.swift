93e2f4f1b3ddbba7fda737b237d52740c2b29b47
Eric Ertl <eric.ertl@mercadolibre.com>
2020-08-13 14:59:04 -0300

    Check empty url (#2297)

--- MercadoPagoSDK/MercadoPagoSDK/Models/BusinessResult/PXBusinessResultViewModel.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Models/BusinessResult/PXBusinessResultViewModel.swift
@@ -320,7 +320,8 @@ extension PXBusinessResultViewModel: PXNewResultViewModelInterface {
         default:
             return nil
         }
-        if let urlString = urlString {
+        if let urlString = urlString,
+            !urlString.isEmpty {
             if appendLanding {
                 let landingURL = MLBusinessAppDataService().appendLandingURLToString(urlString)
                 return URL(string: landingURL)
--- MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/PXResultViewModel.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/PXResultViewModel.swift
@@ -525,7 +525,8 @@ extension PXResultViewModel: PXNewResultViewModelInterface {
         default:
             return nil
         }
-        if let urlString = urlString {
+        if let urlString = urlString,
+            !urlString.isEmpty {
             if appendLanding {
                 let landingURL = MLBusinessAppDataService().appendLandingURLToString(urlString)
                 return URL(string: landingURL)
