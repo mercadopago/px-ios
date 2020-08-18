8d1beb11da79476b25252ad1fb63e7fe274bd888
Eric Ertl <eric.ertl@mercadolibre.com>
2020-08-18 16:59:30 -0300

    rollback testapp changes

--- ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
+++ ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
@@ -35,7 +35,7 @@
 //    [self.checkoutBuilder setPrivateKeyWithKey:@"APP_USR-1505-092415-b89a7cdcec6cc6c3916deab0c56c7136-472129472"];
 
     //  Argentina
-    [self.checkoutBuilder setPrivateKeyWithKey:@"APP_USR-1505-081312-00f32d0d4c42bf39388397b3029bac5f-509936543"];
+    [self.checkoutBuilder setPrivateKeyWithKey:@"APP_USR-7092-091314-cc8f836a12b9bf78b16e77e4409ed873-470735636"];
 
     PXTrackingConfiguration *trackingConfig = [[PXTrackingConfiguration alloc] initWithTrackListener: self flowName:@"instore" flowDetails:nil sessionId:@"3783874"];
     [self.checkoutBuilder setTrackingConfigurationWithConfig: trackingConfig];
@@ -60,7 +60,7 @@
     PXAdvancedConfiguration* advancedConfig = [[PXAdvancedConfiguration alloc] init];
 
     //ONE TAP
-    [advancedConfig setExpressEnabled:NO];
+    [advancedConfig setExpressEnabled:YES];
     
     //ESC
     [advancedConfig setEscEnabled:YES];
