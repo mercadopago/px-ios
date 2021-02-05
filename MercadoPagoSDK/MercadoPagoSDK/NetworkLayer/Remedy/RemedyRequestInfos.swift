76f593b498cf73965c7a689f48072211588eca1b
Matheus <matheusl.martins@mercadolivre.com>
2021-02-05 14:14:39 -0300

    Add remedy service on new way to do requests

---
+++ MercadoPagoSDK/MercadoPagoSDK/NetworkLayer/Remedy/RemedyRequestInfos.swift
@@ -0,0 +1,37 @@
+//
+//  RemedyRequestInfos.swift
+//  MercadoPagoSDKV4
+//
+//  Created by Matheus Leandro Martins on 05/02/21.
+//
+
+enum RemedyRequestInfos {
+    case getRemedy(String, Bool, Data?)
+}
+
+extension RemedyRequestInfos: RequestInfos {
+    var endpoint: String {
+        switch self {
+        case .getRemedy(let paymentId, _, _): return "/px_mobile/v1/remedies/\(paymentId)/"
+        }
+    }
+    
+    var method: HTTPMethodType {
+        .post
+    }
+    
+    var body: Data? {
+        switch self {
+        case .getRemedy(_, _, let body): return body
+        }
+    }
+    
+    
+    var parameters: [String : Any]? {
+        switch self {
+        case .getRemedy(_, let oneTap, _): return [
+            "one_tap" : oneTap ? "true" : "false"
+        ]
+        }
+    }
+}
